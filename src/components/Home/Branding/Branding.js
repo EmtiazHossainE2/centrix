3417d34d3874e9a55669440c11382c9ceae571d7
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-01-19 14:11:03 +0600

    branding section done

--- src/components/Home/Branding/Branding.js
+++ src/components/Home/Branding/Branding.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { brandData } from './brandData'
+import { AiOutlineRight } from 'react-icons/ai'
 import './Branding.css'
+
 
 const Branding = () => {
   return (
-    <div>Branding</div>
+    <div className='branding container mx-auto section__padding grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center'>
+      {brandData.map((data, index) => (
+        <div key={index}>
+          <div className={`branding__content text-white  shadow-xl rounded-lg  md:text-center container mx-auto ${index === 1 ? "activeColor " : " bg-[#16337a] "} `}>
+            <div className='md:flex justify-center'>
+              <img src={data.icon} alt="icon" />
+            </div>
+            <h3 className='text-lg lg:text-2xl font-bold py-3'>{data.title}</h3>
+            <p>{data.des}</p>
+            <div className='md:flex justify-center'>
+              <button className='px-6 py-1 mt-5 border rounded-[50px] text-white text-center flex gap-x-2 justify-center items-center'>See More <AiOutlineRight /></button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
---
+++ src/components/Home/Branding/brandData.js
@@ -0,0 +1,21 @@
+import icon1 from '../../../assets/icon1.png'
+import icon2 from '../../../assets/icon2.png'
+import icon3 from '../../../assets/icon3.png'
+
+export const brandData = [
+  {
+    title: "Automation",
+    icon: icon1,
+    des: "Interdum platea potenti ad efficitur iaculis inceptos sodales eleifend venenatis luctus semper"
+  },
+  {
+    title: "Learning Machine",
+    icon: icon2,
+    des: "Interdum platea potenti ad efficitur iaculis inceptos sodales eleifend venenatis luctus semper"
+  },
+  {
+    title: "Big data",
+    icon: icon3,
+    des: "Interdum platea potenti ad efficitur iaculis inceptos sodales eleifend venenatis luctus semper"
+  }
+]
