fa2a733394f4684498e5537f34bed71668b19c96
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-01-20 19:03:32 +0600

    banner fix

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -4,15 +4,15 @@ import './Banner.css'
 const Banner = () => {
   return (
     <div className='container mx-auto'>
-      <div className='banner section__padding' id='home'>
-        <div className="banner__content ">
-          <h1 className='gradient__text'>Artificial Intelligence for Business Applications</h1>
-          <p className='py-3'>Amet consectetur adipisicing elit. Accusantium, maiores reprehenderit saepe libero veritatis assumenda qui suscipit animi ab. Labore sint dicta corporis placeat nisi beatae repellat ducimus. Quidem sit consequuntur, consequatur non .</p>
+      <div className='section__padding flex flex-col lg:flex-row justify-between items-center space-x-5' id='home'>
+        <div className="basis-1/2 ">
+          <h1 className='text-4xl md:text-5xl font-bold gradient__text pb-5 md:py-5'>Artificial Intelligence for Business Applications</h1>
+          <p className='text-[#81afdd] py-2'>Amet consectetur adipisicing elit. Accusantium, maiores reprehenderit saepe libero veritatis assumenda qui suscipit animi ab. Labore sint dicta corporis placeat nisi beatae repellat ducimus. Quidem sit consequuntur, consequatur non .</p>
           <div className="centrixBtn pt-5 pb-10">
             <button type="button" id='discover'>Discover More</button>
           </div>
         </div>
-        <div className="banner__right "></div>
+        <div className="basis-1/2 "></div>
       </div>
     </div>
   )
